8a99fc46dc720fb10d5f0f5b95eed577b2ab2682
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-30 18:30:00 -0400

    feat: begin examining element selectors

--- lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
+++ lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
@@ -1,17 +1,25 @@
 // Examine document object
 
 // console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-document.title = "Item Lister";
-console.log(document.title);
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = "Item Lister";
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
 // bad way to select stuff because if anything gets
 // added or subtracted index changes
 // document.all[10].textContent = 'Hello';
-console.log(document.forms);
-console.log(document.links);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// Selectors 
+
+// Get Element by ID
+// console.log(document.getElementById('header-title'))
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
