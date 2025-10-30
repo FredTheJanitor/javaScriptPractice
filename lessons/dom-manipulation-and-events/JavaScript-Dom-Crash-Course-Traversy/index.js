70d0b0083d496447a8703a2a50951faa3fa68c67
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-30 07:52:13 -0400

    feat: finish get Element by ID section

--- lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
+++ lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
@@ -21,9 +21,12 @@
 
 // Get Element by ID
 // console.log(document.getElementById('header-title'))
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
+// var headerTitle = document.getElementById('header-title');
+// var header = document.getElementById('main-header');
+// console.log(headerTitle);
 // headerTitle.textContent = 'whaddap';
 // console.log(headerTitle.innerText);
 // console.log(headerTitle.textContent);
-headerTitle.innerHTML = '<h3>Hello</h3>';
+// headerTitle.innerHTML = '<h3>Hello</h3>';
+// header.style.borderBottom = 'solid 3px #000';
+
