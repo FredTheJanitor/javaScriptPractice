49861923d684ad43c4dc0156bfd229d3dec09036
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-30 07:10:46 -0400

    feat: add some basic document observation logs

---
+++ lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
@@ -0,0 +1,17 @@
+// Examine document object
+
+// console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title = "Item Lister";
+console.log(document.title);
+console.log(document.doctype);
+console.log(document.head);
+console.log(document.body);
+console.log(document.all);
+// bad way to select stuff because if anything gets
+// added or subtracted index changes
+// document.all[10].textContent = 'Hello';
+console.log(document.forms);
+console.log(document.links);
