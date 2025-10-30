208a3cf19f80cb60f8c4b6f781facae3c90f22c2
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-10-30 18:30:00 -0400

    feat: add tag name and show functionality of selcr

--- lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
+++ lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
@@ -30,20 +30,39 @@
 // headerTitle.innerHTML = '<h3>Hello</h3>';
 // header.style.borderBottom = 'solid 3px #000';
 
-// Get elementSSSSS by class name
+// // Get elementSSSSS by class name
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent= 'hello 2';
-items[1].style.fontWeight= 'bold';
-items[1].style.backgroundColor = 'yellow';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent= 'hello 2';
+// items[1].style.fontWeight= 'bold';
+// items[1].style.backgroundColor = 'yellow';
 
-// this gives an error because
-// items is an HTMLCollections (kinda like a node list)
-// each item has to be affected individually so great for loop opp
-// items.style.backgroundColor = '#f4f4f4';
+// // this gives an error because
+// // items is an HTMLCollections (kinda like a node list)
+// // each item has to be affected individually so great for loop opp
+// // items.style.backgroundColor = '#f4f4f4';
 
-for(i=0;i<items.length;i++) {
-    items[i].style.backgroundColor= '#f4f4f4'
-}
+// for(i=0;i<items.length;i++) {
+//     items[i].style.backgroundColor= '#f4f4f4'
+// }
+
+// // get elements by tag name
+
+// // same thing just with html tag name in this case 'li' for list items
+// var items = document.getElementsByTagName('li');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent= 'hello 2';
+// items[1].style.fontWeight= 'bold';
+// items[1].style.backgroundColor = 'yellow';
+
+// // this gives an error because
+// // items is an HTMLCollections (kinda like a node list)
+// // each item has to be affected individually so great for loop opp
+// // items.style.backgroundColor = '#f4f4f4';
+
+// for(i=0;i<items.length;i++) {
+//     items[i].style.backgroundColor= '#f4f4f4'
+// }
