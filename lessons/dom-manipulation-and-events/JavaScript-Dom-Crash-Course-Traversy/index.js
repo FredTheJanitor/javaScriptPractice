ca87ab411b42d2c4f84f913676960ef184e79d38
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-01 14:04:17 -0400

    feat: do some more eventwerk and stuff my homie

--- lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
+++ lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
@@ -231,13 +231,41 @@ var itemList = document.querySelector('#items');
 
 // EVENT TYPES:
 
-var button = document.getElementById('button')
+var button = document.getElementById('button');
+var box = document.getElementById('box');
 
 // button.addEventListener("click", runEvent);
 // button.addEventListener("dblclick", runEvent);
 // button.addEventListener("mousedown", runEvent);
-button.addEventListener("mouseup", runEvent);
+// button.addEventListener("mouseup", runEvent);
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', runEvent);
+
+// box.addEventListener('mouseover', runEvent);
+// box.addEventListener('mouseout', runEvent);
+
+// box.addEventListener('mousemove', runEvent);
+
+var itemInput = document.querySelector('input[type="text"]');
+var form = document.querySelector('form');
+
+// itemInput.addEventListener("keydown", runEvent);
+// itemInput.addEventListener("keyup", runEvent);
+// itemInput.addEventListener("keypress", runEvent);
+
+// itemInput.addEventListener("focus", runEvent);
+// itemInput.addEventListener("blur", runEvent);
+
+// itemInput.addEventListener("cut", runEvent);
+// itemInput.addEventListener("paste", runEvent);
+
+itemInput.addEventListener("input", runEvent);
 
 function runEvent(e){
     console.log('Event Type: '+e.type);
+    console.log(e.target.value);
+    document.getElementById('output').innerHTML = '<h3>'+e.target.value+'</h3>'
+    // output.innerHTML ='<h3>MouseX: '+e.offsetX+' </h3><h3>MouseY: '+e.offsetY+'</h3>'
+    // document.body.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+",0)";
 }
