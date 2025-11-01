bd350e292642c6ab803883607f9f5b11177b305e
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-01 08:02:04 -0400

    feat: consolidate loop

--- lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
+++ lessons/dom-manipulation-and-events/JavaScript-Dom-Crash-Course-Traversy/index.js
@@ -95,12 +95,8 @@ console.log(titles);
 titles[0].textContent = 'Hello';
 
 var odd = document.querySelectorAll('li:nth-child(odd)')
-
-for(var i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = '#f4f4f4';
-}
-
 var even = document.querySelectorAll('li:nth-child(even)')
 for(var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = '#f4f4f4';
     even[i].style.backgroundColor = 'rgba(175, 175, 175, 1)';
 }
